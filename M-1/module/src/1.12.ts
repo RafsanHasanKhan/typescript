02ab5c0d7fd3ee122963020b1a449788d2d8f5a4
rafsanhasankhan <rafsanhasankhaninfo@gmail.com>
2025-01-29 13:00:29 +0600

    file change

---
+++ M-1/module/src/1.12.ts
@@ -0,0 +1,12 @@
+{
+
+
+  const searchName = (value: string) => {
+    if (value) {
+      console.log('Searching');
+    } else {
+      console.log('there is nothing to search');
+    }
+  }
+  searchName('rafsan')
+}
