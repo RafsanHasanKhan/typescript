97be25b38cf198da0a29e51687790becd1f87a95
rafsanhasankhan <rafsanhasankhaninfo@gmail.com>
2025-01-29 09:37:08 +0600

    first commit

---
+++ M-1/Typescript/module/src/1.4.ts
@@ -0,0 +1,30 @@
+// Basic Data Type
+
+// String
+
+let firstName: string = 'rafsan';
+// Number
+let roll: number = 123;
+// Boolean
+let isAdmin: boolean = true;
+// undefined
+let x: undefined = undefined;
+// null
+let y: null = null;
+
+let d:number;
+
+d = 123;
+
+
+// Array
+let friends: string[] = ['rafsan', 'hasan', 'khan'];
+friends.push('rifat')
+let eligibleRollList: number[] = [1, 2, 3, 4, 5];
+eligibleRollList.push(7);
+
+// tuple ---> array ---> order ---> type of values
+
+let coordinates : [number, number] = [1, 5]
+
+let ageName : [number, string] = [5,'rafsan']
